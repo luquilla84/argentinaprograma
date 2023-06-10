3a570b19c5b9f520ff1fdc0f5882c50323e31845
lucas <lucassebastianbrocanelli@gmail.com>
2023-06-10 20:11:05 -0300

    Agregonel archivo conversionGrado.js

---
+++ practico0/conversionGrados.js
@@ -0,0 +1,9 @@
+//Punto A=
+let fahrenheit = 50;
+let celcius = (fahrenheit - 32) * 5/9;
+console.log(celcius);
+
+//PuntoB=
+fahrenheit = 70;
+celcius =(fahrenheit - 32) * 5/9;
+console.log(celcius);
