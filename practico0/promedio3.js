8ec1a8e68b080b2bf6eee8bafafb9f888a68adbc
lucas <lucassebastianbrocanelli@gmail.com>
2023-06-10 20:13:32 -0300

    Practico N0 fib¿nalizado

---
+++ practico0/promedio3.js
@@ -0,0 +1,20 @@
+// Punto A=
+let num = 5;
+let num1 = 7;
+let num2 = 9;
+let promedio = (num + num1 + num2)/3;
+console.log (promedio)
+
+//Punto B=
+num = -2;
+num1 = -3;
+num2 = -8;
+promedio = (num + num1 + num2) / 3;
+console.log(promedio);
+
+// Punto C=
+num = -1.2;
+num1 = 5.3;
+num2 = -3.3;
+promedio = (num + num1 + num2) / 3;
+console.log(promedio);
