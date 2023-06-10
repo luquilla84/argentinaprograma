d170ec18e155750d10c3f3ed6ec4505f36491c0d
lucas <lucassebastianbrocanelli@gmail.com>
2023-06-10 18:43:03 -0300

    dos ejercicios

---
+++ practico0/multiplicacion.js
@@ -0,0 +1,8 @@
+let num1 = 35;
+let num2 = 84;
+let num3 = 0.5;
+let multiplicacion = num1 * num2 * num3;
+console.log (multiplicacion);
+
+
+//si multiplicamos 2* 17 * 6, nos da de resultado 204;
---
+++ practico0/resta.js
@@ -0,0 +1,6 @@
+let minuendo = 30;
+let sustraendo = 15;
+let resta = minuendo - sustraendo;
+console.log(resta);
+
+//si el minuendo es 10 y el sustraendo es 2, el resultado es 8.
